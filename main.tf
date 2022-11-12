c331e03285c6febefeff4b75e3a9abc116711601
Mark Farnsworth <farnsworth2008@gmail.com>
2022-11-12 14:54:41 +0000

    0.1.21 Updated `site_group` module
    - Updated `site_group` module

--- main.tf
+++ main.tf
@@ -49,7 +49,7 @@ module "site" {
 # tfsec:ignore:aws-ec2-no-public-egress-sgr
 # tfsec:ignore:aws-ec2-no-public-ingress-sgr
 module "site_group" {
-  source = "github.com/s3d-club/terraform-aws-site-group?ref=v0.1.42"
+  source = "github.com/s3d-club/terraform-aws-site-group?ref=v0.1.43"
 
   cidr6s        = ["::/0"]
   cidrs         = ["0.0.0.0/0"]
